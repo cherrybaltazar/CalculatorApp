3c214e223bb2a386d81dc332f92b0dcbec7a945f
Cherry <baltazarcherrylyn@gmail.com>
2019-05-06 17:59:35 +0800

    * Added Operators * Clears the Screen when operation has been made

--- Calculator App/ViewController.swift
+++ Calculator App/ViewController.swift
@@ -36,7 +36,12 @@ class ViewController: UIViewController {
     @IBOutlet weak var btnOne: UIButton!
     @IBOutlet weak var btnZero: UIButton!
     
-    var arrSymbols = ["AC","=","+/-","%","/","x","-","+","."]
+    var arrSymbols = ["AC","=","+/-","%","."]
+    var arrOperators = ["/","x","-","+"]
+    var result = Double()
+    var firstOperand = Double()
+    var secondOperand = Double()
+    var operatorValue = String()
     var arrCollectedStrings = [String]()
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -44,56 +49,88 @@ class ViewController: UIViewController {
      
     }
     @IBAction func calculate(_ sender: UIButton) {
-  
-        
-       
         if txtViewResult.text.isEmpty {
             //Clear all the collections in array and display a number in the first element of array
-            arrCollectedStrings.removeAll()
+            firstOperand = 0
             arrCollectedStrings.append((sender.titleLabel?.text)!)
             txtViewResult.text = arrCollectedStrings[0]
             
               //It checks the collected string if it is a number or a symbol. If it is a symbol it will proceed. If the symbol is a point, it will insert 0 in the first element of array. Otherwise, it will clear the screen.
-            for symbol in arrSymbols {
-                if arrCollectedStrings.last == symbol {
+            if arrSymbols.contains((sender.titleLabel?.text)!) || arrOperators.contains((sender.titleLabel?.text)!) {
                     if arrCollectedStrings.last == arrSymbols.last {
                         arrCollectedStrings.insert("0", at: 0)
                         txtViewResult.text = arrCollectedStrings.joined()
+                        sender.tag = 5
                     } else {
                         arrCollectedStrings.removeAll()
+                        firstOperand = 0
+                        secondOperand = 0
+                        result = 0
+                        operatorValue = ""
                         txtViewResult.text = ""
                     }
-                }
             }
-            
         } else {
-            //Clears the screen if the symbol is AC
+            //AC
             if sender.titleLabel?.text == arrSymbols.first {
                 arrCollectedStrings.removeAll()
+                firstOperand = 0
+                secondOperand = 0
+                result = 0
+                operatorValue = ""
                 txtViewResult.text = ""
-            } else if (arrCollectedStrings.last != nil) {
-                arrCollectedStrings.append((sender.titleLabel?.text)!)
-                txtViewResult.text = arrCollectedStrings.joined()
-                
-                //Checks if the symbol is the same with a point or equal, it will remove the last element if the point has been tapped
-                if (arrSymbols.last == arrCollectedStrings.last) || arrCollectedStrings.last == arrSymbols[1] {
-                    arrCollectedStrings.removeLast()
-                    
-                    if (arrSymbols[1] == sender.titleLabel?.text) {
-                         arrCollectedStrings.removeLast()
-                    }
-                    
-                    txtViewResult.text = arrCollectedStrings.joined()
-                //Checks if the symbol is +/- or %, it will display zero
-                } else if (arrSymbols[2] == sender.titleLabel?.text || arrSymbols[3] == sender.titleLabel?.text) {
-                    arrCollectedStrings.removeAll()
-                    arrCollectedStrings.insert("0", at: 0)
-                    txtViewResult.text = arrCollectedStrings.joined()
-                }
             } else {
-                //Checks the point symbol if it exist on the screen when you first open the app
-                if (sender.titleLabel?.text == arrSymbols.last) {
-                    arrCollectedStrings.insert("0" + (sender.titleLabel?.text)!, at: 0)
+                //OPERATORS
+                if (arrOperators.contains((sender.titleLabel?.text)!)){
+                    print(arrCollectedStrings, "= nil( /, x, - +)")
+                    operatorValue = (sender.titleLabel?.text)!
+                    //ADD VALUE TO FIRST OPERAND
+                    if sender.tag <= 1  {
+                        firstOperand = Double(arrCollectedStrings.joined()) ?? 0
+                        txtViewResult.text = String(firstOperand)
+                        arrCollectedStrings.removeAll()
+                        print(firstOperand, "FIRST OPERAND")
+                        sender.tag = 1
+                    }
+                  
+                //EQUALS
+                } else if (sender.tag <= 2 && arrSymbols[1] == sender.titleLabel?.text) {
+                    //ADD VALUE TO SECOND OPERAND
+                    secondOperand = Double(arrCollectedStrings.joined())!
+                   
+                    switch operatorValue {
+                    //DIVIDE
+                    case arrOperators.first:
+                        result = Double(firstOperand) / Double(arrCollectedStrings.joined())!
+                        print(String(result), "DIVIDE")
+                    //MULTIPLY
+                    case arrOperators[1]:
+                        result = Double(firstOperand) * Double(arrCollectedStrings.joined())!
+                        print(String(result), "MULTIPLY")
+                    //SUBTRACT
+                    case arrOperators[2]:
+                        result = Double(firstOperand) - Double(arrCollectedStrings.joined())!
+                        print(String(result), "SUBTRACT")
+                    //ADDITION
+                    case arrOperators[3]:
+                        result = firstOperand + secondOperand
+                        print(String(result), "ADD")
+                    default:
+                       // txtViewResult.text = arrCollectedStrings.joined()
+                        print(String(result), "NONE")
+                    }
+                    secondOperand = firstOperand
+                    firstOperand = result
+                    txtViewResult.text = String(result)
+                    sender.tag = 2
+                    
+                    
+                //NUMBERS & SYMBOLS
+                } else {
+                    if sender.tag == 0 {
+                    arrCollectedStrings.append((sender.titleLabel?.text)!)
+                        print("APPEND A NUMBER")
+                    }
                     txtViewResult.text = arrCollectedStrings.joined()
                 }
             }
