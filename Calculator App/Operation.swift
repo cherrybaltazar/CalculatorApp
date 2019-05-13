8616cc093804c58e9c04775a4cc136f06f22be9f
Cherry <baltazarcherrylyn@gmail.com>
2019-05-13 11:08:07 +0800

    Added operation class

---
+++ Calculator App/Operation.swift
@@ -0,0 +1,10 @@
+//
+//  Operation.swift
+//  Calculator App
+//
+//  Created by ADMIN on 5/13/19.
+//  Copyright © 2019 Cherrylyn Baltazar. All rights reserved.
+//
+
+import Foundation
+import UIKit
